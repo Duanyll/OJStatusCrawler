6742fb9afc99b6f1888e51bff6de9d3f6436125d
duanyll <xiaoguo2004@sina.cn>
2018-12-08 22:09:46 +0800

    添加从命令行读取选项的功能

--- OJStatusCrawler/Program.cs
+++ OJStatusCrawler/Program.cs
@@ -1,25 +1,72 @@
 ﻿using System;
 using System.IO;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
 
 namespace OJStatusCrawler
 {
     class Program
     {
+        const string HelpText = @"
+命令行使用方法:
+user:   指定用户名,若不指定,则从stdin读入
+oj:     指定OJ,若不指定,则从stdin读入
+            目前支持bzoj,hdu
+output: 指定输出文件(默认result.md),附加到文件末尾
+
+示例:
+    dotnet run user=llf0703 oj=bzoj
+        抓取llf0703的bzoj上的AC提交记录.
+";
         static void Main(string[] args)
         {
-            Console.WriteLine("请输入用户名:");
-            string UserName = Console.ReadLine();
-            var crawler = new HDUCrawler();
+            if(args.Length >= 1 && args[0] == "help"){
+                Console.Write(HelpText);
+                return;
+            }
+            var Config = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string,string>(){
+                    {"user",""},
+                    {"oj",""},
+                    {"output","result.md"}
+                })
+                .AddCommandLine(args)
+                .Build();
+
+            string UserName;
+            if(Config["user"] == ""){
+                Console.WriteLine("请输入用户名:");
+                UserName = Console.ReadLine();
+            }else{
+                UserName = Config["user"];
+            }
+
+            string OJName;
+            if(Config["oj"] == ""){
+                Console.WriteLine("请输入OJ代码:");
+                OJName = Console.ReadLine();
+            }else{
+                OJName = Config["oj"];
+            }
+
+            OJCrawler crawler;
+            switch(OJName){
+                case "bzoj":
+                    crawler = new BZOJCrawler();
+                    break;
+                case "hdu":
+                    crawler = new HDUCrawler();
+                    break;
+                default:
+                    Console.WriteLine("OJ代码未知或尚未支持");
+                    return;
+            }
             var list = crawler.SearchForACStatus(UserName);
             Console.WriteLine("抓取完毕");
-            foreach(var i in list)
-            {
-                Console.WriteLine(i.ProblemID);
-            }
-            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"result.md", true))
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(Config["output"], true))
             {
                 file.WriteLine();
-                file.WriteLine($"# {UserName}的AC提交记录");
+                file.WriteLine($"# {UserName}在{OJName}上的AC提交记录");
                 file.WriteLine($"抓取时间:{DateTime.Now}");
                 file.WriteLine("|题目编号|链接|");
                 file.WriteLine("|-|-|");
@@ -29,6 +76,7 @@ namespace OJStatusCrawler
                 }
                 file.WriteLine("----------------------------------");
             }
+            Console.WriteLine($"已写入结果到{Config["output"]}");
             Console.WriteLine("按任意键退出");
             Console.ReadKey();
         }
--- README.md
+++ README.md
@@ -2,7 +2,20 @@
 
 基于.net core的爬虫，主要用于爬取大佬的提交记录，用于了解大佬的做题顺序。
 
-目前已支持抓取HDU与BZOJ的AC记录,将会保存到`./result.md`中
+目前已支持抓取HDU与BZOJ的AC记录
+
+## 使用方法
+
+```
+user:   指定用户名,若不指定,则从stdin读入
+oj:     指定OJ,若不指定,则从stdin读入
+            目前支持bzoj,hdu
+output: 指定输出文件(默认result.md),附加到文件末尾
+
+示例:
+    dotnet run user=llf0703 oj=bzoj
+        抓取llf0703的bzoj上的AC提交记录.
+```
 
 ## 建议了解
 
