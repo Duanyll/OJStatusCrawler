3d9b050c81b28342e9f961248af48da207000696
duanyll <xiaoguo2004@sina.cn>
2018-12-08 21:32:48 +0800

    添加写结果到文件

--- OJStatusCrawler/Program.cs
+++ OJStatusCrawler/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace OJStatusCrawler
 {
@@ -15,6 +16,20 @@ namespace OJStatusCrawler
             {
                 Console.WriteLine(i.ProblemID);
             }
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"result.md", true))
+            {
+                file.WriteLine();
+                file.WriteLine($"# {UserName}的AC提交记录");
+                file.WriteLine($"抓取时间:{DateTime.Now}");
+                file.WriteLine("|题目编号|链接|");
+                file.WriteLine("|-|-|");
+                foreach (var item in list)
+                {
+                    file.WriteLine($"|{item.ProblemID}|[点击查看]({item.URL})|");
+                }
+                file.WriteLine("----------------------------------");
+            }
+            Console.WriteLine("按任意键退出");
             Console.ReadKey();
         }
     }
