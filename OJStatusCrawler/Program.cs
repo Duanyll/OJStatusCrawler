fb404bd67a076472db449b52061d23333cefff16
duanyll <xiaoguo2004@sina.cn>
2018-12-09 13:23:51 +0800

    支持反转题目顺序和去重

--- OJStatusCrawler/Program.cs
+++ OJStatusCrawler/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace OJStatusCrawler
 {
@@ -13,6 +14,8 @@ user:   指定用户名,若不指定,则从stdin读入
 oj:     指定OJ,若不指定,则从stdin读入
             目前支持bzoj,hdu
 output: 指定输出文件(默认result.md),附加到文件末尾
+mode:   firstac(默认),ac,all(尚未支持)
+reverse:是否倒序输出(默认true)
 
 示例:
     dotnet run user=llf0703 oj=bzoj
@@ -20,37 +23,47 @@ output: 指定输出文件(默认result.md),附加到文件末尾
 ";
         static void Main(string[] args)
         {
-            if(args.Length >= 1 && args[0] == "help"){
+            if (args.Length >= 1 && args[0] == "help")
+            {
                 Console.Write(HelpText);
                 return;
             }
             var Config = new ConfigurationBuilder()
-                .AddInMemoryCollection(new Dictionary<string,string>(){
+                .AddInMemoryCollection(new Dictionary<string, string>(){
                     {"user",""},
                     {"oj",""},
-                    {"output","result.md"}
+                    {"output","result.md"},
+                    {"mode","firstac"},
+                    {"reverse","true"}
                 })
                 .AddCommandLine(args)
                 .Build();
 
             string UserName;
-            if(Config["user"] == ""){
+            if (Config["user"] == "")
+            {
                 Console.WriteLine("请输入用户名:");
                 UserName = Console.ReadLine();
-            }else{
+            }
+            else
+            {
                 UserName = Config["user"];
             }
 
             string OJName;
-            if(Config["oj"] == ""){
+            if (Config["oj"] == "")
+            {
                 Console.WriteLine("请输入OJ代码:");
                 OJName = Console.ReadLine();
-            }else{
+            }
+            else
+            {
                 OJName = Config["oj"];
             }
 
             OJCrawler crawler;
-            switch(OJName){
+            switch (OJName)
+            {
                 case "bzoj":
                     crawler = new BZOJCrawler();
                     break;
@@ -61,7 +74,44 @@ output: 指定输出文件(默认result.md),附加到文件末尾
                     Console.WriteLine("OJ代码未知或尚未支持");
                     return;
             }
-            var list = crawler.SearchForACStatus(UserName);
+
+            List<StatusLog> list;
+            switch(Config["mode"]){
+                case "ac":
+                    list = crawler.SearchForACStatus(UserName);
+                    break;
+                case "firstac":
+                    var tmp = crawler.SearchForACStatus(UserName);
+                    tmp.Reverse();
+                    var solved = new HashSet<string>();
+                    list = new List<StatusLog>();
+                    foreach(var i in tmp){
+                        if(!solved.Contains(i.ProblemID)){
+                            solved.Add(i.ProblemID);
+                            list.Add(i);
+                        }
+                    }
+                    list.Reverse();
+                    break;
+                case "all":
+                    Console.WriteLine("尚未支持该抓取模式");
+                    return;
+                default:
+                    Console.WriteLine("未知抓取模式");
+                    return;
+            }
+
+            switch(Config["reverse"]){
+                case "false":
+                    break;
+                case "true":
+                    list.Reverse();
+                    break;
+                default:
+                    Console.WriteLine("reverse参数应为true或false");
+                    return;
+            }
+
             Console.WriteLine("抓取完毕");
             using (System.IO.StreamWriter file = new System.IO.StreamWriter(Config["output"], true))
             {
--- README.md
+++ README.md
@@ -2,15 +2,18 @@
 
 基于.net core的爬虫，主要用于爬取大佬的提交记录，用于了解大佬的做题顺序。
 
-目前已支持抓取HDU与BZOJ的AC记录
+目前已支持抓取HDU与BZOJ的AC记录,POJ这段时间都无法访问,暂不添加
 
 ## 使用方法
 
 ```
+命令行使用方法:
 user:   指定用户名,若不指定,则从stdin读入
 oj:     指定OJ,若不指定,则从stdin读入
             目前支持bzoj,hdu
 output: 指定输出文件(默认result.md),附加到文件末尾
+mode:   firstac(默认),ac,all(尚未支持)
+reverse:是否倒序输出(默认true)
 
 示例:
     dotnet run user=llf0703 oj=bzoj
